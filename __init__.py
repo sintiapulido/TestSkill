eae8d652246a19b245e67cdacb14246ac3d80667
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-10-26 13:37:22 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -46,15 +46,17 @@ class HelloWorldSkill(MycroftSkill):
     @intent_handler(IntentBuilder('ThankYouIntent').require('ThankYouKeyword'))
     def handle_thank_you_intent(self, message):
         """ This is an Adapt intent handler, it is triggered by a keyword."""
-        self.speak_dialog("welcome")
         ser.write(b"Ass\n")
+        self.speak_dialog("welcome")
+        
 
     @intent_handler('HowAreYou.intent')
     def handle_how_are_you_intent(self, message):
         """ This is a Padatious intent handler.
         It is triggered using a list of sample phrases."""
-        self.speak_dialog("how.are.you")
         ser.write(b"Ass\n")
+        self.speak_dialog("how.are.you")
+        
 
     @intent_handler(IntentBuilder('HelloWorldIntent')
                     .require('HelloWorldKeyword'))
@@ -63,8 +65,9 @@ class HelloWorldSkill(MycroftSkill):
         the skills.log file."""
         self.log.info("There are five types of log messages: "
                       "info, debug, warning, error, and exception.")
-        self.speak_dialog("hello.world")
         ser.write(b"Ass\n")
+        self.speak_dialog("hello.world")
+        
 
     def stop(self):
         pass
