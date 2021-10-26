ad8838fd2e2555a124404e1151d5ed51fadf1de3
ashtonpersaud <92948501+ashtonpersaud@users.noreply.github.com>
2021-10-26 19:37:13 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,26 +12,22 @@ class GoKnightsSkill(MycroftSkill):
 
     def initialize(self):
         my_setting = self.settings.get('my_setting')
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        ser.flush()
 
     @intent_handler('knights.intent')
     def handle_not_are_you_intent(self, message):
         self.speak_dialog("Charge on")
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        ser.flush()
         ser.write(b"charge")
         
     @intent_handler('greeting.intent')
     def handle_not_are_you_intent(self, message):
         self.speak_dialog("greeting")
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        ser.flush()
         ser.write(b"wave")
         
     @intent_handler('knights.intent')
     def handle_not_are_you_intent(self, message):
         self.speak_dialog("Charge on")
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        ser.flush()
         ser.write(b"charge")
 
     def stop(self):
